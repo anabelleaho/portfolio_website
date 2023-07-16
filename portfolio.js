9c400c7e9de8067a4225d25483e68e9e18cb3b91
Anabelle Ho <anabelleho@hotmail.com>
2023-07-16 17:58:14 +0800

    new changes made to homepage

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// First, get a reference to the image.
+const image = document.querySelector('avatar');
+
+// Then, define a function that toggles a "glow" CSS class.
+function toggleGlow() {
+  image.classList.toggle('glow');
+}
+
+// Finally, add an event listener that toggles the class when the image is clicked.
+image.addEventListener('click', toggleGlow);
